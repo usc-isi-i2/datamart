c0cbfcf86c73ae12e535db0ee3704144e326fb44
Sukriti <sukriti>
2018-11-01 12:43:26 -0700

    market Materialzer

---
+++ datamart/materializers/tradingeconomics_market_materializer.py
@@ -0,0 +1,82 @@
+from datamart.materializers.materializer_base import MaterializerBase
+
+import pandas as pd
+import typing
+import sys
+import traceback
+
+
+class TradingEconomicsMarketMaterializer(MaterializerBase):
+    """TradingEconomicsMaterializer class extended from  Materializer class
+
+    """
+
+    def __init__(self):
+        """ initialization and loading the city name to city id map
+
+        """
+        MaterializerBase.__init__(self)
+        self.key = None
+
+    def get(self,
+            metadata: dict = None,
+            variables: typing.List[int] = None,
+            constrains: dict = None
+            ) -> typing.Optional[pd.DataFrame]:
+        """ API for get a dataframe.
+
+            Args:
+                metadata: json schema for data_type
+                variables:
+                constrains: include some constrains like date_range, location and so on
+
+                Assuming date is in the format %Y-%m-%d
+        """
+        if not constrains:
+            constrains = dict()
+
+        materialization_arguments = metadata["materialization"].get("arguments", {})
+        getUrl = metadata['url']
+        if "key" in constrains:
+            self.key = {"key": constrains["key"]}
+        else:
+            self.headers = {"key": "guest:guest"}
+        date_range = constrains.get("date_range", {})
+        d1=""
+        datestr=""
+        if date_range["start"]:
+            datestr+="d1="+ date_range["start"]
+        if date_range["end"]:
+            datestr += '&d2='+date_range["end"]
+        if date_range["start"]:
+            path=getUrl.split("&")
+            path[1]=datestr
+            getUrl="&".join(path)
+
+        datasetConfig = {
+            "where_to_download": {
+                "frequency": "quarterly",
+                "method": "get",
+                "file_type": "csv",
+                "template": getUrl,
+                "replication": {
+                },
+                "identifier": metadata['title'].replace(' ', '_')
+            },
+        }
+
+        return self.fetch_data(getUrl, datasetConfig)
+
+    def fetch_data(self, getUrl, datasetConfig):
+        """
+        Returns:
+             result: A pd.DataFrame;
+        """
+        try:
+            data = pd.read_csv(getUrl, encoding='utf-16')
+            return data
+        except Exception as e:
+            print('exception in run', e)
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            print(''.join(lines))
