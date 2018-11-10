0ed249f9cce9e7c90718dff181959e7aa8ae511d
RqS <shaorunqi@gmail.com>
2018-11-09 19:08:02 -0800

    update joiner interface

--- datamart/joiners/rltk_joiner.py
+++ datamart/joiners/rltk_joiner.py
@@ -12,7 +12,15 @@ class RLTKJoiner(JoinerBase):
     def __init__(self):
         pass
 
-    def join(self, left_df, right_df, left_columns, right_columns) -> pd.DataFrame:
+    def join(self,
+             left_df: pd.DataFrame,
+             right_df: pd.DataFrame,
+             left_columns: list,
+             right_columns: list,
+             left_metadata: dict,
+             right_metadata: dict,
+             ) -> pd.DataFrame:
+
         return pd.merge(left=left_df,
                         right=right_df,
                         left_on=[left_df.columns[idx] for idx in left_columns],
