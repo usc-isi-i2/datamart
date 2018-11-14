a09e68bab9baaa648b79e18bb5e2657324fcc5b2
Sukriti Sharma <sukritis@usc.edu>
2018-11-14 14:13:11 -0800

    unit test case for trading economics materializer

---
+++ datamart/unit_tests/test_tradingeconomics_materializer.py
@@ -0,0 +1,35 @@
+from datamart.materializers.tradingeconomics_materializer import TradingEconomicsMaterializer
+import unittest, json, os
+import pandas as pd
+from datamart.utils import Utils
+
+resources_path = os.path.join(os.path.dirname(__file__), "./resources")
+
+
+class TestWikidataMaterializer(unittest.TestCase):
+    def setUp(self):
+        self.tradingeconomics_materializer = TradingEconomicsMaterializer()
+
+    @Utils.test_print
+    def test_get(self):
+        mock_metadata = {
+            'title':"GDP",
+            "url": "https://api.tradingeconomics.com/historical/country/all/indicator/gdp%20growth%20annual?c=guest:guest&format=csv",
+            "materialization": {
+                "arguments": {
+                }
+            }
+        }
+        fake_constrains = {
+            "date_range": {
+                "start": None,
+                "end": None
+            },
+            "named_entity": {0: ["Albania","Argentina","Angola"]}
+        }
+
+        result = self.tradingeconomics_materializer.get(metadata=mock_metadata,constrains=fake_constrains).to_dict(orient="records")
+        sample_result = pd.read_csv(os.path.join(resources_path, 'tradingeconomics_gdp.csv'), encoding='utf-16')
+        sample_result = sample_result.fillna('').to_dict(orient="records")
+
+        self.assertEqual(result, sample_result)
