4bfdbd7739c9714403e6b842b30f563b0baead4b
RqS <shaorunqi@gmail.com>
2018-11-15 11:51:07 -0800

    updates for worldbank

--- datamart/materializers/worldbank_materializer.py
+++ datamart/materializers/worldbank_materializer.py
@@ -1,7 +1,6 @@
 from datamart.materializers.materializer_base import MaterializerBase
-import datetime
+import dateutil.parser as parser
 import requests
-import typing
 import os
 import json
 import pandas as pd
@@ -19,8 +18,6 @@ class WorldBankMaterializer(MaterializerBase):
         with open(os.path.join(resources_path, 'country_to_id.json'), 'r') as json_file:
             reader = json.load(json_file)
             self.country_to_id_map = reader
-            countrynames = self.country_to_id_map.keys()
-            self.DEFAULT_LOCATIONS = list(countrynames)
 
     def get(self, metadata: dict = None, constrains: dict = None) -> pd.DataFrame:
         if not constrains:
@@ -36,12 +33,40 @@ class WorldBankMaterializer(MaterializerBase):
     def fetch_data(self, date_range: dict = None, locations: list = None, dataset_id: str = None):
         if date_range:
             start_date = date_range.get("start", None)
+            if start_date:
+                start_year = parser.parse(start_date).year
             end_date = date_range.get("end", None)
+            if end_date:
+                end_year = parser.parse(end_date).year
+
+        URL_ind_metadata = 'https://api.worldbank.org/v2/indicators/' + dataset_id + '?format=json'
+        response_metadata = requests.get(url=URL_ind_metadata)
+        json_respose_metadata = json.loads(response_metadata.content)
+        json_respose_metadata = json_respose_metadata[1][0]
+        sourceNote = json_respose_metadata['sourceNote']
+        sourceOrganization = json_respose_metadata['sourceOrganization']
+
         if not locations:
-            locations = self.DEFAULT_LOCATIONS
+            if not date_range:
+                URL_ind = 'https://api.worldbank.org/v2/countries/' + "ALL" + '/indicators/' + dataset_id + '?format=json'
+            else:
+                URL_ind = 'https://api.worldbank.org/v2/countries/' + "ALL" + '/indicators/' + dataset_id + '?format=json&date=' + str(start_year) + ':' + str(end_year)
+            response_ind = requests.get(url=URL_ind)
+            json_respose_ind = json.loads(response_ind.content)
 
-        appended_data = []
+            pages_per_ind = json_respose_ind[0]['pages']
+            all_data = []
+            for i in range(1, pages_per_ind + 1):
+                p = {'page': i}
+                response_pagewise = requests.get(url=URL_ind, params=p)
+                json_pagewise = json.loads(response_pagewise.content)
+                all_data.extend(json_pagewise[1])
+            appended_data = pd.io.json.json_normalize(all_data)
+            appended_data['sourceNote'] = sourceNote
+            appended_data['sourceOrganization'] = sourceOrganization
+            return appended_data
 
+        appended_data = None
         for location in locations:
             location_id = self.country_to_id_map.get(location, None)
             if location_id is None:
@@ -61,14 +86,10 @@ class WorldBankMaterializer(MaterializerBase):
                 json_pagewise = json.loads(response_pagewise.content)
                 all_data.extend(json_pagewise[1])
             df = pd.io.json.json_normalize(all_data)
-            URL_ind_metadata = 'https://api.worldbank.org/v2/indicators/' + dataset_id + '?format=json'
-            response_metadata = requests.get(url=URL_ind_metadata)
-            json_respose_metadata = json.loads(response_metadata.content)
-            json_respose_metadata = json_respose_metadata[1][0]
-            sourceNote = json_respose_metadata['sourceNote']
-            sourceOrganization = json_respose_metadata['sourceOrganization']
             df['sourceNote'] = sourceNote
             df['sourceOrganization'] = sourceOrganization
-            appended_data.append(df)
-        appended_data = pd.concat(appended_data, axis=0, ignore_index=True)
+            if appended_data is None:
+                appended_data = df.copy()
+            else:
+                appended_data = pd.concat([appended_data, df], axis=0, ignore_index=True)
         return appended_data
