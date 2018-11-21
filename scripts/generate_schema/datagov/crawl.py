763c4660c6042375d78943581a4ef4fe8f874e88
Chen <Chen@guest-131.isi.edu>
2018-11-20 16:27:22 -0800

    update the script for datagov

--- scripts/generate_schema/datagov/crawl.py
+++ scripts/generate_schema/datagov/crawl.py
@@ -1,8 +1,10 @@
 import urllib.request as urlRequest
 import urllib.parse as urlParse
 import json
+import sys
 
 
+args = sys.argv[1]
 url = "http://catalog.data.gov/api/3/action/package_search?rows=1000&start="
 # pretend to be a chrome 47 browser on a windows 10 machine
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
@@ -14,7 +16,7 @@ for num in range(0,303):
 	x = urlRequest.urlopen(req)
 
 	response_dict = json.loads(x.read())
-	with open('output'+str(num)+'.jsonl', 'w') as outfile:
+	with open(str(args)+'/output'+str(num)+'.jsonl', 'w') as outfile:
 	    for entry in response_dict['result']['results']:
 	    	for entry2 in entry['resources']:
 	    		if entry2['mimetype']!= None:
