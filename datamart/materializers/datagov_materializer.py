3e60a3a339d06b634f3628ea4043fe4493ebb8a5
RqS <shaorunqi@gmail.com>
2018-12-03 18:38:41 -0800

    update env, fix bug

--- datamart/materializers/datagov_materializer.py
+++ datamart/materializers/datagov_materializer.py
@@ -1,15 +1,13 @@
 from datamart.materializers.materializer_base import MaterializerBase
-import json 
 import requests
 import io
 import pandas as pd
+import typing
 
 
 class datagov_materializer(MaterializerBase):
-    def __init__(self):
-        MaterializerBase.__init__(self)
-
-
+    def __init__(self, **kwargs):
+        MaterializerBase.__init__(self, **kwargs)
 
     def get(self,
             metadata: dict = None,
@@ -22,7 +20,7 @@ class datagov_materializer(MaterializerBase):
                 constrains: include some constrains like date_range, location and so on
         """
         args = metadata['materialization']['arguments']
-        database=requests.get(args).content 
-        df=pd.read_csv(io.StringIO(database))
-        
+        database = requests.get(args).content
+        df = pd.read_csv(io.StringIO(database.decode('utf-8')))
+
         return df
