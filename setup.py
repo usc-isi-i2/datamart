298357d22880a197e69c23b85b01dcfad4e36d2c
Lituta <lituta@outlook.com>
2019-01-10 11:05:35 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Datamart',
+      version='1.0',
+      description='Data Augmentation',
+      author='ISI',
+      url='https://github.com/usc-isi-i2/datamart/tree/development',
+      packages=['datamart'],
+     )
