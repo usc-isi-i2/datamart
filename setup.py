bdfaa11e0932c83a4d1c9b1bb0fd9872dd4880c3
Lituta <lituta@outlook.com>
2019-01-10 12:16:28 -0800

    update setup.py to install dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from distutils.core import setup
 
+
+with open('requirements.txt', 'r') as f:
+    install_requires = list()
+    for line in f:
+        re = line.strip()
+        if re:
+            install_requires.append(re)
+
+
 setup(name='Datamart',
-      version='1.0',
+      version='1.0dev',
       description='Data Augmentation',
       author='ISI',
       url='https://github.com/usc-isi-i2/datamart/tree/development',
       packages=['datamart'],
-     )
+      install_requires=install_requires)
