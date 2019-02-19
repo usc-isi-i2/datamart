2ce5504353c41bc53285c9ba86a08f6d8f830c81
Juan C. Roldán <juancarlos@sevilla.es>
2019-02-19 14:55:57 -0800

    Improved unit test

--- datamart/unit_tests/test_wikitables_materializer.py
+++ datamart/unit_tests/test_wikitables_materializer.py
@@ -1,6 +1,7 @@
 from datamart.materializers.wikitables_materializer import WikitablesMaterializer
-import unittest, os
-import pandas as pd
+from unittest import TestCase
+from os.path import join, dirname
+from pandas import read_csv
 from datamart.utilities.utils import Utils
 
 resources_path = os.path.join(os.path.dirname(__file__), "./resources")
@@ -16,13 +17,13 @@ class TestWikitablesMaterializer(unittest.TestCase):
             "materialization": {
                 "arguments": {
                     "url": "https://en.wikipedia.org/wiki/Albedo",
-                    "xpath": "/html[1]/body[1]/div[3]/div[3]/div[4]/div[1]/table[2]"
+                    "xpath": "/html[1]/body[1]/div[3]/div[3]/div[4]/div[1]/table[1]"
                 }
             }
         }
 
         result = self.wikitables_materializer.get(metadata=mock_metadata).to_dict(orient="records")
-        sample_result = pd.read_csv(os.path.join(resources_path, 'wikitables_sample.csv'), dtype=str)
+        sample_result = read_csv(os.path.join(resources_path, 'wikitables_sample.csv'), dtype=str)
         sample_result = sample_result.fillna('').to_dict(orient="records")
 
         self.assertEqual(result, sample_result)
