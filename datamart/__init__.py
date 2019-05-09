977fad2dfd34a3386831b2ca546be7a7e5c23f63
ckxz105 <liu494@usc.edu>
2019-05-09 10:20:01 -0700

    update init

--- datamart/__init__.py
+++ datamart/__init__.py
@@ -1,5 +1,5 @@
 name = "datamart"
 DEFAULT_URL = "https://isi-datamart.edu"
-from datamart.entries import search, augment, join, download
-from datamart.dataset import Dataset
-from datamart.stateless_entries_url_upload import generate_metadata, bulk_generate_metadata, upload, bulk_upload, wikipedia_tables_metadata
+# from datamart.entries import search, augment, join, download
+# from datamart.dataset import Dataset
+# from datamart.stateless_entries_url_upload import generate_metadata, bulk_generate_metadata, upload, bulk_upload, wikipedia_tables_metadata
